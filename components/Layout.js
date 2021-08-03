4ea779676a16bed9706091a2403de2fa74aeb6eb
acr-coder <kegitimi@gmail.com>
2021-08-03 12:57:43 +0300

    lastlastlast

--- components/Layout.js
+++ components/Layout.js
@@ -5,10 +5,9 @@ const Layout = ({ children }) => {
   return (
     <>
       <Head>
-        <meta charSet="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Note App</title>
+        <title>Task App</title>
       </Head>
       <Navbar />
       {children}
