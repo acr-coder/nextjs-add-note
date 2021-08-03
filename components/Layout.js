f6d6dd8f48faf67f961acb4ac3b199733583cb62
acr-coder <kegitimi@gmail.com>
2021-08-03 10:49:24 +0300

    bg

--- components/Layout.js
+++ components/Layout.js
@@ -1,17 +1,19 @@
-import Head from 'next/head';
-import Navbar from './Navbar';
+import Head from "next/head";
+import Navbar from "./Navbar";
 
 const Layout = ({ children }) => {
-    return(
-        <>
-        <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <title>Note App</title>
-        </Head>
-        <Navbar/>
-        { children }
-        </>
-    )
-}
+  return (
+    <>
+      <Head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Note App</title>
+      </Head>
+      <Navbar />
+      {children}
+    </>
+  );
+};
 
 export default Layout;
